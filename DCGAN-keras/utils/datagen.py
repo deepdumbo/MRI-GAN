d83a7ef2309df1a619dbbfeb3feea896b63d340e
Ravnoor Gill <ravnoor@gmail.com>
2017-08-24 17:04:27 -0400

    2d to 3d #11

--- DCGAN-keras/utils/datagen.py
+++ DCGAN-keras/utils/datagen.py
@@ -33,8 +33,8 @@ class MyDict(dict):
 class TwoImageIterator(Iterator):
     """Class to iterate A and B images at the same time."""
 
-    def __init__(self, directory, a_dir_name='A', b_dir_name='B', dim_ordering='th', N=-1,
-                 batch_size=32, target_size, shuffle=True, seed=None):
+    def __init__(self, directory, target_size, a_dir_name='A', b_dir_name='B', dim_ordering='th', N=-1,
+                 batch_size=32, shuffle=True, seed=None):
         """
         Iterate through two directories at the same time.
 
